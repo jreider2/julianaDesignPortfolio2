3be543f1fbe06fe3cfa94847c25782fba9cbf2b2
Juliana R <jreider@luc.edu>
2024-09-10 11:45:53 -0700

    update reflections in thoughtfilter
    fix the copy in reflections for thoughtfilter

--- components/constants/thoughtFilterConstants.js
+++ components/constants/thoughtFilterConstants.js
@@ -8,16 +8,9 @@ export const text = <>{overviewTextP1} <br/><br/> {overviewTextP2} <br/><br/> {o
 
 
 export const takeaways = [
-    <div><span className={`emphasis`}>Collaborative Synergy </span>Reflecting on the continuous back-and-forth with the development team, I've come to appreciate how vital it is to sync design thinking with technical possibilities from the get-go. This collaboration wasn't just about making things easier; it was crucial for bringing our design visions to life in a practical, functional way.
-    If i put the in i need to have to think of an example.
-     It was a reminder of how pivotal real user feedback is to crafting solutions that genuinely resonate.</div>,
+    <div><span className={`emphasis`}>Collaborative Synergy: </span>Reflecting on the continuous back-and-forth with the development team, I've come to appreciate how vital it is to sync design thinking with technical possibilities from the get-go. This collaboration wasn't just about making things easier; it was crucial in order to bring the design vision to life in a practical, functional way.</div>,
 
-    <div><span className={`emphasis`}>Deep Dives into User Needs: </span>The extensive user interviews deepened my understanding of what content marketers truly need, guiding the tool’s design from the start to better suit their daily challenges.
-    f i put the in i need to have to think of an example.</div>,
+    <div><span className={`emphasis`}>Deep Dives into User Needs: </span>The extensive user interviews deepened my understanding of what content marketers truly need, guiding the tool’s design from the start to better suit their daily challenges. It was a reminder of how pivotal user research and domain knowledge is to crafting solutions that genuinely resonate.</div>,
 
-    <div><span className={`emphasis`}>Evolving with Feedback. </span>The journey from initial concept to final design was filled with pivotal decisions, especially about aesthetics and user interface simplicity. Adapting our approach based on user feedback really highlighted the fluid nature of design, where you start with a design language that evolves, sometimes drastically, to meet real-world needs and preferences.</div>,
-
-    <div><span className={`emphasis`}>Something from the heart. </span> tbd</div>,
-
-    <div><span className={`emphasis`}>Something from the heart. </span> tbd </div>,
+    <div><span className={`emphasis`}>Evolving with Feedback: </span>The journey from initial concept to final design was filled with pivotal decisions, especially about aesthetics and user interface simplicity. Adapting our approach based on user feedback really highlighted the fluid nature of design, where you start with a design language that evolves, sometimes drastically, to meet real-world needs and preferences.</div>,
   ];

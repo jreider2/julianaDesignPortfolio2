7f383a2ef550a9d1467302f76d7d1182808c8d76
Juliana R <jreider@luc.edu>
2024-10-28 11:56:31 -0700

    Add back in deprecated property for now and teaser card

--- components/teasercard.js
+++ components/teasercard.js
@@ -33,7 +33,7 @@ export default function TeaserCard({ title, description, image, link }) {
           src={image}
           alt="Case Study Image"
           sizes="100vw"
-          responsive // Use responsive layout
+          layout="responsive" // Use responsive layout
           width={100}
           height={100}
           style={{

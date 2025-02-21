1a90505dade4fc7009c6bd647a6774eaf84a70b7
Juliana R <jreider@luc.edu>
2025-02-21 15:00:32 -0800

    Part one in mission overhaul

--- pages/index.js
+++ pages/index.js
@@ -24,52 +24,52 @@ export default function Home({ allPostsData }) {
           <title>{siteTitle}</title>
           {/* <link rel="preload" href="/fonts/MuseoModerno.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/SFProRounded.woff2" as="font" type="font/woff2" crossOrigin="anonymous" /> */}
-          <link rel="preload" as="image" href="/images/lightCirlce_mainPage.webp" />
+          {/* <link rel="preload" as="image" href="/images/lightCirlce_mainPage.webp" /> */}
         </Head>
         <div className={styles.heroContainer}>
           <div className={styles.heroSection}>
-            <Image
-              src="/images/lightCirlce_mainPage.webp"
-              alt="Hero Background"
-              fill
-              style={{ objectFit: 'cover', objectPosition: 'center bottom' }}
-              priority
-            />
-            <motion.h1
-              initial={{ opacity: 0, y: 5 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-            >
-              <span className={styles.EngineersLens}>Engineer's Lens</span><br />
-              <span className={styles.designerVision}>Designer's Vision</span>
-            </motion.h1>
+            <div className={styles.blob1}></div>
+            <div className={styles.blob2}></div>
+            <div className={styles.blob3}></div>
+            <div className={styles.contentWrapper}>
+              <motion.h1
+                initial={{ opacity: 0, y: 5 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+              >
+                <span className={styles.EngineersLens}>Engineer's Lens</span><br />
+                <span className={styles.designerVision}>Designer's Vision</span>
+              </motion.h1>
 
-            <motion.div className={styles.subheadContainer}
-              initial={{ opacity: 0, y: 5 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: 1, duration: 1 }}
-            >
-              <h2>hi! i’m juliana, an engineer turned product designer.</h2>
-              <h2 className={styles.subhead2}>my methodical nature merges aesthetics with functionality.</h2>
-            </motion.div>
+              <motion.div className={styles.subheadContainer}
+                initial={{ opacity: 0, y: 5 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 1, duration: 1 }}
+              >
+                <h2>hi! i'm juliana, an engineer turned product designer.</h2>
+                <h2 className={styles.subhead2}>my methodical nature merges aesthetics with functionality.</h2>
+              </motion.div>
 
-            <motion.div onClick={() => scrollToWork()}
-              initial={{ y: 10 }}
-              animate={{ y: 0 }}
-              transition={{ delay: 2, duration: 0.5, repeat: Infinity, repeatType: "reverse" }}
-              className={arrowStyles.arrowDown}>↓</motion.div>
+              <motion.div onClick={() => scrollToWork()}
+                initial={{ y: 10 }}
+                animate={{ y: 0 }}
+                transition={{ delay: 2, duration: 0.5, repeat: Infinity, repeatType: "reverse" }}
+                className={arrowStyles.arrowDown}>↓</motion.div>
+            </div>
           </div>
-        </div>
+        
         <div className={styles.ghost} ref={workRef}></div>
 
-        <TeaserCard title="Canopy" description="Coming Soon: A cryptocurrency investment and goal setting tool." image="/images/canopyCover.png" link={"/bloom"}></TeaserCard>
+        <div className={styles.teaserCardsContainer}>
+          <TeaserCard title="Canopy" description="Coming Soon: A cryptocurrency investment and goal setting tool." image="/images/canopyCover.png" link={"/bloom"}></TeaserCard>
 
-        <TeaserCard title="Convoscope" description="Augmenting cross-disciplinary collaboration for researchers with AI." image="/images/convoscopeCover.png" link={"/convoScope"}></TeaserCard>
+          <TeaserCard title="Convoscope" description="Augmenting cross-disciplinary collaboration for researchers with AI." image="/images/convoscopeCover.png" link={"/convoScope"}></TeaserCard>
 
-        <TeaserCard title="Comet" description="Using design to reimagine source control management." image="/images/cometTeaser.webp" link={"/comet"}></TeaserCard>
+          <TeaserCard title="Comet" description="Using design to reimagine source control management." image="/images/cometTeaser.webp" link={"/comet"}></TeaserCard>
 
-        <TeaserCard title="Thought Filter" description="First designer for a generative AI SaaS tool for freelance writers and creators." image="/images/editWithAiCurvedCorners.png" link={"/thoughtFilter"}></TeaserCard>
-
+          <TeaserCard title="Thought Filter" description="First designer for a generative AI SaaS tool for freelance writers and creators." image="/images/editWithAiCurvedCorners.png" link={"/thoughtFilter"}></TeaserCard>
+        </div>
+    </div>
       </Layout >
     </>
   );

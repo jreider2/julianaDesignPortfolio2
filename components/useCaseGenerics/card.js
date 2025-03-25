20a7078a949f6d12ef0e15b7dae69bf8578df149
Juliana R <jreider@luc.edu>
2025-03-25 11:57:13 -0700

    autoplay video on canopy page

--- components/useCaseGenerics/card.js
+++ components/useCaseGenerics/card.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Image from "next/image";
 import styles from "./card.module.css";
-import useCaseStyles from "../convoscope/useCase.module.css";
 
-export default function Card ({ className, p1, bullets, image1, image2, video1, children}) {
+export default function Card ({ className, p1, bullets, image1, image2, video1, autoPlay = false, children}) {
     return (
         <div className={`${styles.outterContainer} ${className}`}>
             {p1 && (
@@ -14,7 +13,14 @@ export default function Card ({ className, p1, bullets, image1, image2, video1, 
             
             {video1 && (
                 <div className={styles.imageWrapper}>
-                    <video controls loop muted width="100%" height="auto">
+                    <video 
+                        controls 
+                        loop 
+                        muted 
+                        autoPlay={autoPlay}
+                        width="100%" 
+                        height="auto"
+                    >
                         <source src={video1} type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
